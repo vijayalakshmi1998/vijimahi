51d574ff25eb06d32a47b49bcaf2a5d6f1da6458
vijayalakshmi1998 <31558978+vijayalakshmi1998@users.noreply.github.com>
2017-09-02 15:11:18 +0530

    Create prime bw two.c

---
+++ prime bw two.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int num1,num2,i,flag=0;
+if(num1<num2)
+{
+for(i=2;num1<=num1%2;num1++)
+{
+flag=1;
+break;
+}
+}
+if(flag==0)
+{
+printf("%d",num1);
+++num1
+}
+}
