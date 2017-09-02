9134611b03cac2c8d4a58463b6dff642f0eb83d4
vijayalakshmi1998 <31558978+vijayalakshmi1998@users.noreply.github.com>
2017-09-02 12:17:15 +0530

    Update alphabet.c

--- alphabet.c
+++ alphabet.c
@@ -2,13 +2,13 @@
 int main()
 {
     char c;
-    printf("Enter a character: ");
     scanf("%c",&c);
-
-    if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
-        printf("%c is an alphabet.",c);
+    if((c>='a' && c<='z')||(c>='A' && c<='Z'))
+    {
+        printf("alphabet");
+    }
     else
-        printf("%c is not an alphabet.",c);
-
-    return 0;
-}
+    {
+        printf("not an alphabet");
+    }
+    
