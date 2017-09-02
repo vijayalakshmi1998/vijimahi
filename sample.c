3976fe5fa78b95f02f5b12a6428f07b1d85eb62f
vijayalakshmi1998 <31558978+vijayalakshmi1998@users.noreply.github.com>
2017-09-02 11:05:22 +0530

    Rename README.md to sample.c

---
+++ sample.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+scanf("%d",&a);
+if(a<0)
+{
+printf("negative");
+}
+else if(a>0)
+{
+printf("positive");
+}
+else
+{
+printf("zero");
+}
+}
