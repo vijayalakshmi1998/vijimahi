2b82b6bc7e9927140f124ce37b52baaada43c304
vijayalakshmi1998 <31558978+vijayalakshmi1998@users.noreply.github.com>
2017-09-02 15:44:07 +0530

    Create fact.c

---
+++ fact.c
@@ -0,0 +1,19 @@
+#include<sdtio.h>
+#include<conio.h>
+int main()
+{
+int num,i,fact=1;
+scanf("%d",&num);
+if(num<0)
+{
+printf("error");
+}
+else
+{
+for(i=1;i<=n;i++)
+{
+fact=fact*i;
+printf("%d",fact);
+}
+}
+}
